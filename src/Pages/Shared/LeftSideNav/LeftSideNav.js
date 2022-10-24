5fc5bf9708cdd56e8b920813ec8f0be8f60780f4
rawfoon <mgrawfoon@gmail.com>
2022-10-24 23:22:31 +0600

    left side nav data issue fixed

--- src/Pages/Shared/LeftSideNav/LeftSideNav.js
+++ src/Pages/Shared/LeftSideNav/LeftSideNav.js
@@ -6,7 +6,7 @@ const LeftSideNav = () => {
     const [categories, setCategories] = useState([])
 
     useEffect(()=>{
-        fetch('https://dragon-news-server-rawfoon.vercel.appfi/news-categories')
+        fetch('https://dragon-news-server-rawfoon.vercel.app/news-categories')
         .then(res => res.json())
         .then(data => setCategories(data))
     },[])
